ac4076d9875287c6a88c64b6a19266182863b437
kadiado <marcothulio2@gmail.com>
2022-05-14 21:14:04 -0300

    subindo arquivos

---
+++ app.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('.quiz-form');
+const finalResult = document.querySelector('.result')
+
+const correctAnswers = ['D', 'B', 'C', 'A'];
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	
+	let score = 0;
+	const userAnswers = [
+			form.inputQuestion1.value,
+			form.inputQuestion2.value,
+			form.inputQuestion3.value,
+			form.inputQuestion4.value
+	]
+	
+	userAnswers.forEach((userAnswer, index) => {
+		if (userAnswer === correctAnswers[index]) {
+			score += 25
+		}
+	})
+
+	scrollTo(0, 164)
+	
+	let counter = 0
+	
+	const timer = setInterval(() => {
+		if  (counter === score ) {
+			clearInterval(timer)
+		};
+		finalResult.querySelector('span').textContent = `${counter}%`
+	counter++
+		finalResult.classList.remove('d-none')
+	}, 50);
+	
+});
+
+const divForm = document.querySelector('.my-5')
+
+
+
+// setTimeout(() => {
+// 	alert('oi')
+// }, 2000)
