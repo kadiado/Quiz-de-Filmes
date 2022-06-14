59ae91e055bf81abc9a213e273fdcd4dc590acee
kadiado <marcothulio2@gmail.com>
2022-06-14 13:03:42 -0300

    add

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const finalScoreContainer = document.querySelector(".final-score-container");
 const correctAnswers = ["D", "B", "C", "A"];
 let score = 0;
 
-const getUsersAnswer = () => correctAnswers.map((_, index) =>
-    form[`inputQuestion${index + 1}`].value)
-
+const getUsersAnswer = () => correctAnswers.map((_, index) => form[`inputQuestion${index + 1}`].value);
 
 const calculateUserScore = userAnswers => {
   userAnswers.forEach((userAnswer, index) => {
-    const ifUserAnswerCorrect = userAnswer === correctAnswers[index]
+    const ifUserAnswerCorrect = userAnswer === correctAnswers[index];
 
     if (ifUserAnswerCorrect) {
       score += 25;
@@ -22,8 +20,8 @@ const showFinalScore = () => {
   scrollTo({
     top: 164,
     left: 0,
-    behavior: 'smooth'
-  })
+    behavior: "smooth",
+  });
   finalScoreContainer.classList.remove("d-none");
 };
 
@@ -39,26 +37,84 @@ const animateFinalScore = () => {
 };
 
 const resetUserScore = () => {
-  score = 0
-}
+  score = 0;
+};
 
 form.addEventListener("submit", event => {
   event.preventDefault();
 
-
   // Reseta a pontuação antes de enviar o form novamente
-  resetUserScore()
+  resetUserScore();
 
   // obtem as resposts do usuario
-  const userAnswers = getUsersAnswer()
+  const userAnswers = getUsersAnswer();
 
   // Calcula pontuação das respostas
-  calculateUserScore(userAnswers)
+  calculateUserScore(userAnswers);
 
   // exibe a pontuação e sobre a tela
-  showFinalScore()
-  
-  // anima pontuação 
-  animateFinalScore()
+  showFinalScore();
 
+  // anima pontuação
+  animateFinalScore();
+});
+const form = document.querySelector(".quiz-form");
+const finalScoreContainer = document.querySelector(".final-score-container");
+
+const correctAnswers = ["D", "B", "C", "A"];
+let score = 0;
+
+const getUsersAnswer = () => correctAnswers.map((_, index) => form[`inputQuestion${index + 1}`].value);
+
+const calculateUserScore = userAnswers => {
+  userAnswers.forEach((userAnswer, index) => {
+    const ifUserAnswerCorrect = userAnswer === correctAnswers[index];
+
+    if (ifUserAnswerCorrect) {
+      score += 25;
+    }
+  });
+};
+
+const showFinalScore = () => {
+  scrollTo({
+    top: 164,
+    left: 0,
+    behavior: "smooth",
+  });
+  finalScoreContainer.classList.remove("d-none");
+};
+
+const animateFinalScore = () => {
+  let counter = 0;
+
+  const timer = setInterval(() => {
+    if (counter === score) {
+      clearInterval(timer);
+    }
+    finalScoreContainer.querySelector("span").textContent = `${counter++}%`;
+  }, 20);
+};
+
+const resetUserScore = () => {
+  score = 0;
+};
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  // Reseta a pontuação antes de enviar o form novamente
+  resetUserScore();
+
+  // obtem as resposts do usuario
+  const userAnswers = getUsersAnswer();
+
+  // Calcula pontuação das respostas
+  calculateUserScore(userAnswers);
+
+  // exibe a pontuação e sobre a tela
+  showFinalScore();
+
+  // anima pontuação
+  animateFinalScore();
 });
